2ccab34510a89bc2b4e5b98159ca8dca0af43c87
Aman Kumar <aman140426@gmail.com>
2025-08-10 11:49:46 +0530

    by size

--- GRAPH/DisjointSet.cpp
+++ GRAPH/DisjointSet.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 class DisjointSet
 {
-    vector<int> rank, parent;
+    vector<int> rank, parent, size;
 
 public:
     DisjointSet(int n)
     {
         rank.resize(n + 1, 0);
+        size.resize(n + 1, 1);
         parent.resize(n + 1);
         for (int i = 0; i <= n; i++)
         {
@@ -46,6 +47,25 @@ public:
             rank[ulp_u]++;
         }
     }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+
+        if (ulp_v == ulp_u)
+            return;
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
 };
 
 int main()
@@ -72,5 +92,28 @@ int main()
     }
     else
         cout << "Not same\n";
+
+    DisjointSet ds1(7);
+    ds1.unionBySize(1, 2);
+    ds1.unionBySize(2, 3);
+    ds1.unionBySize(4, 5);
+    ds1.unionBySize(6, 7);
+    ds1.unionBySize(5, 6);
+    // check if 3 and 7 same or not
+    if (ds1.findParent(3) == ds1.findParent(7))
+    {
+        cout << "Same\n";
+    }
+    else
+        cout << "Not same\n";
+
+    ds1.unionByRank(3, 7);
+    if (ds1.findParent(3) == ds1.findParent(7))
+    {
+        cout << "Same\n";
+    }
+    else
+        cout << "Not same\n";
+
     return 0;
 }

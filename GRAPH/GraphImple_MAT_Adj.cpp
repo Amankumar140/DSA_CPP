22ca575be2f4e5bb35e5a67ec0db6da4a6b610fc
Aman Kumar <aman140426@gmail.com>
2025-10-25 23:44:26 +0530

    revision of graph

---
+++ GRAPH/GraphImple_MAT_Adj.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <queue>
+
+void addEdge(vector<int> adj[], int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void adjacencyMatrix(vector<vector<int>> &list, int u, int v)
+{
+    list[u][v] = 1;
+    list[v][u] = 1;
+}
+
+void printGraph(vector<int> adj[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << " : ";
+        for (int j : adj[i])
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+void printMatrix(vector<vector<int>> &mat)
+{
+    for (int i = 0; i < mat.size(); i++)
+    {
+        cout << i << " : ";
+        for (int j = 0; j < mat[i].size(); j++)
+        {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+// for adjacency list
+void bfsTraversal(vector<int> adj[], int n, int s)
+{
+    vector<bool> visited(n + 1, false);
+    queue<int> q;
+    q.push(s);
+    visited[s] = true;
+    while (!q.empty())
+    {
+        int curr = q.front();
+        q.pop();
+        cout << curr << " ";
+        for (int y : adj[curr])
+        {
+            if (!visited[y])
+            {
+                visited[y] = true;
+                q.push(y);
+            }
+        }
+    }
+}
+
+// for adjacency matrix
+void bfsTraversal2(vector<vector<int>> &mat, int s)
+{
+    int n = mat.size();
+    vector<bool> visited(n + 1, false);
+    visited[s] = true;
+    queue<int> q;
+    q.push(s);
+    while (!q.empty())
+    {
+        int curr = q.front();
+        q.pop();
+        cout << curr << " ";
+        for (int i = 0; i < n; i++)
+        {
+            if (mat[curr][i] == 1 && !visited[i])
+            {
+                visited[i] = true;
+                q.push(i);
+            }
+        }
+    }
+}
+
+// for disconnected graph
+
+// two functions used....
+
+
+int main()
+{
+    int n = 4;
+    vector<int> adj[4];
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 2);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 1, 3);
+    vector<vector<int>> mat(n, vector<int>(n, 0));
+    adjacencyMatrix(mat, 0, 1);
+    adjacencyMatrix(mat, 0, 2);
+    adjacencyMatrix(mat, 1, 2);
+    adjacencyMatrix(mat, 1, 3);
+    // printGraph(adj, n);
+    // cout << "AdjancyMatrix" << endl;
+    // printMatrix(mat);
+    bfsTraversal(adj, n, 0);
+    cout << endl;
+    bfsTraversal2(mat, 0);
+    return 0;
+}
--- segmentTree/SegmentTree.cpp
+++ segmentTree/SegmentTree.cpp
@@ -1,60 +1,51 @@
-#include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
-int a[100005], seg[4 * 100005];
+#include <bits/stdc++.h>
 
-void build(int ind, int low, int high)
+int arr[100], seg[4 * 100];
+
+void build(int idx, int low, int high)
 {
     if (low == high)
     {
-        seg[ind] = a[low];
+        seg[idx] = arr[low];
         return;
     }
-
     int mid = (low + high) / 2;
-    // recursive call
-    build(2 * ind + 1, low, mid);
-    build(2 * ind + 2, mid + 1, high);
-    // backtracking
-    seg[ind] = max(seg[2 * ind + 1], seg[2 * ind + 2]);
+    build(2 * idx + 1, low, mid);
+    build(2 * idx + 2, mid + 1, high);
+    seg[idx] = max(seg[2 * idx + 1], seg[2 * idx + 2]);
 }
 
-int query(int ind, int low, int high, int l, int r)
+int query(int idx, int low, int high, int l, int r)
 {
     // completely inside
     if (low >= l && high <= r)
     {
-        return seg[ind];
+        return seg[idx];
     }
-    // completely outside
+    // outside
     if (high < l || low > r)
-    {
         return INT_MIN;
-    }
-    // partial lies like overlaps
-    // goes left and right
-    int mid = (high + low) / 2;
-    int left = query(2 * ind + 1, low, mid, l, r);
-    int right = query(2 * ind + 2, mid + 1, high, l, r);
+
+    // overlapping
+    int mid = (low +  high) / 2;
+    int left = query(2 * idx + 1, low, mid, l, r);
+    int right = query(2 * idx + 2, mid + 1, high, l, r);
     return max(left, right);
 }
 
 int main()
 {
     int n;
+    cout << "Enter n :";
     cin >> n;
+
     for (int i = 0; i < n; i++)
     {
-        cin >> a[i];
+        cin >> arr[i];
     }
-
     build(0, 0, n - 1);
-    int q; // queries
-    cin >> q;
-    while (q--)
-    {
-        int l, r;
-        cin >> l >> r;
-        cout << query(0, 0, n - 1, l, r);
-    }
+    cout << query(0, 0, n - 1, 2, 3) << endl;
+    return 0;
 }
